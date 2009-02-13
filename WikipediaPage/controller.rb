ac1635ad16cd7764072921f9a0493019aa1bb8f0
Vidal Graupera <vidal@vdggroup.com>
2009-02-13 15:08:16 -0800

    wip on new apis and history

--- WikipediaPage/controller.rb
+++ WikipediaPage/controller.rb
@@ -2,73 +2,75 @@ require 'rho/rhocontroller'
 
 class WikipediaPageController < Rho::RhoController
 
+  # GET /WikipediaPage/index
   def index
-    puts "WikipediaPage index"
-    # force fetch of daily wikipedia homepage
-    if @params["home"]
-      wiki_get("::Home")
-    end
+    puts "WikipediaPage index with params=#{@params.inspect.to_s}"
     
+    @search = @params["search"] || "::Home"
+
     render
   end
   
+  # GET /WikipediaPage/show
+  # this is rendered inside the iframe
   def show
-    puts "WikipediaPage show"
+    puts "WikipediaPage show with params=#{@params.inspect.to_s}"
     
-    @pages = WikipediaPage.find(:all)
-    puts @pages.length
+    @search = @params['search']  || "::Home"
     
-    @page = @pages[0]
+    wiki_get(@search)
+
     puts @page.inspect.to_s
     
-    # show contents, otherwise query for homepage
+    # show contents if available
     if @page
-      #if there is record but no data then sync in not complete
-      @data = @page.data ? @page.data.unpack("m")[0] : "Requesting..."
-      SyncEngine::dosync
+      @data = @page.data.unpack("m")[0]
     else
+      # no page yet....
       @data = "Please wait..."
-      wiki_get("::Home")
     end
 
     render :action => :show, :layout => false
   end
   
+  # WikipediaPage/{my page}/fetch
   def fetch
-    puts "WikipediaPage fetch"
+    puts "WikipediaPage fetch with params=#{@params.inspect.to_s}"
       
+    # strip braces
     wiki_get(@params['id'])
 
-    redirect :action => :index
+    redirect :action => :index, :params => {:search => @params['id']}
   end
   
-  def create
-    puts "WikipediaPage create"
-      
-    wiki_get(@params['search'])
-
-    redirect :action => :index
+   # GET /WikipediaPage/history
+  def history
+    @pages = WikipediaPage.find(:all, {:order => :created_at})
   end
   
   protected
   
   def wiki_get(article)
-    puts "wiki_get(#{article})\n"
+    puts "WikipediaPageController wiki_get(#{article})\n"
     
-    @page = WikipediaPage.new(:search => article)
-    @page.save
-
-    WikipediaPage.set_notification("/Wikipedia/WikipediaPage")
-
-    success = SyncEngine::login('anonymous', 'password')
-    if success
-      SyncEngine::dosync
+    object_id = "data_#{article}"
+    @page = WikipediaPage.find(object_id)
+    
+    if @page
+      puts "++++++Cache hit for #{article}"
+      puts @page.inspect.to_s
     end
+    
+    unless @page
+      puts "------Cache miss for #{article}"
+      WikipediaPage.set_notification("/Wikipedia/WikipediaPage?search=#{article}")
 
-    # if SyncEngine::logged_in == 0
-    #   SyncEngine::login('wikipedia', 'doesnotmatter')
-    # end
-    # 
-    # WikipediaPage.ask(article)
+      # make sure we are logged in, this user must exist in rhosync or sync will fail
+      if SyncEngine::logged_in == 0
+        SyncEngine::login('anonymous', 'password')
+      end
+    
+      WikipediaPage.ask(article)
+    end
   end
 end
