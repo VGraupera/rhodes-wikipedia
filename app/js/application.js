431d514f4ce1954d979da5dff6681480a4732dca
Hampton Catlin <hcatlin@greed-2.local>
2009-03-22 23:37:08 -0400

    history links still broken

--- app/js/application.js
+++ app/js/application.js
@@ -2,7 +2,6 @@ var toolbarOptionalButton = function() {
   $.get("/app/WikipediaPage/history", function(data) {
     $('#history_box').html(data).show('fast');
     $('#wrapper').hide();
-    $('a').hide();
     return 'false';
   })
 }
